da4bcc46f329b54245b0fa6d7524bd49baa99040
LAPTOP-I7BH2PQ8\Liuxk <liu_xingkang@icekredit.com>
2021-01-25 17:12:27 +0800

    no message

--- src/common/until.js
+++
@@ -1,8 +0,0 @@
-//生成指定长度的唯一ID
-export function GenNonDuplicateID(randomLength) {
-    return Number(
-        Math.random()
-        .toString()
-        .substr(3, randomLength) + Date.now()
-    ).toString(36);
-}
