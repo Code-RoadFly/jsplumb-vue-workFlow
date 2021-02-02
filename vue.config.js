e5106beeec02e1a21bbac8ee3c98d970cc7f0a12
LAPTOP-I7BH2PQ8\Liuxk <liu_xingkang@icekredit.com>
2021-02-02 17:00:34 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-
+const path = require('path')
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
 module.exports = {
-  publicPath: './'
+  publicPath: '/jsplumb-vue-wordFlow',
+  chainWebpack: config => {
+    config.resolve.alias.set('@', resolve('src'))
+  },
+  productionSourceMap: false
 }
